c06444719e8ee0a01ae6218d38d62062ab7e2bb4
Daniel Ghenghea <daniel.ghenghea2000@gmail.com>
2025-10-19 18:56:46 +0300

    add a few test cases for a2.l3

--- tests/a2/test.a2.l3.cpp
+++ tests/a2/test.a2.l3.cpp
@@ -110,3 +110,145 @@ Test test_a2_l3_collapse_edge_edge_boundary("a2.l3.collapse_edge.edge.boundary",
 
 	expect_collapse(mesh, edge, after);
 });
+
+/*
+EDGE CASE EASY
+
+Initial mesh:
+
+    1
+   /|
+  / |
+0   |
+  \ |
+	 \| 
+ 	 	2 
+
+Collapse edge 0-2
+
+Resulting mesh:
+
+    1
+   /
+  / 
+0
+
+REJECT!
+*/
+
+Test test_a2_l3_collapse_edge_edge_case_easy("a2.l2.collapse_edge.edge.case.easy", []() {
+	Halfedge_Mesh mesh = Halfedge_Mesh::from_indexed_faces({
+		             Vec3(-1.0f, 1.1f, 0.0f),
+		Vec3(-2.1f, 0.3f, 0.0f),
+		            Vec3(-1.1f, -1.1f, 0.0f)
+	}, {
+		{0, 2, 1},
+	});
+	Halfedge_Mesh::EdgeRef edge = mesh.halfedges.begin()->edge;
+
+	if (mesh.collapse_edge(edge)) {
+		throw Test::error("collapse_edge should not work.");
+	}
+});
+
+/*
+EDGE CASE HARD
+
+Initial: 
+
+0---1---2
+|   |   |
+|  /4\  |
+ /  |  \|
+3\  |  /5
+| \ 6/  |
+|   |   |
+7---8---9
+
+Collapse edge 4--6
+
+Correct result:
+
+0---1---2
+|   |   |
+|   |   |
+|  /3\  |
+4/  |  \5
+|   |   |
+6---7---8
+*/
+Test test_a2_l1_collapse_edge_hard_1("a2.l2.collapse_edge.hard.1", []() {
+	Halfedge_Mesh mesh = Halfedge_Mesh::from_indexed_faces({
+	Vec3(-1.0f, 1.1f, 0.0f), Vec3(0.5f, 1.0f, 0.0f), Vec3(1.1f, 0.9f, 0.0f),
+													 Vec3(0.6f, 0.4f, 0.0f),
+	Vec3(-1.1f, -0.3f, 0.0f)                       , Vec3(1.2f, -0.2f, 0.0f),
+													 Vec3(0.4f, -0.2f, 0.0f),										
+	Vec3(-1.2f,-0.7f, 0.0f), Vec3(0.3f, -0.8f, 0.0f), Vec3(1.15f, -1.0f, 0.0f)
+	}, {
+		{3, 6, 4},
+		{0, 3, 4, 1},
+		{1, 4, 5, 2},
+		{4, 6, 5},
+		{3, 7, 8, 6},
+		{6, 8, 9, 5}
+	});
+
+	Halfedge_Mesh::EdgeRef edge = mesh.halfedges.begin()->next->edge;
+
+	Halfedge_Mesh after = Halfedge_Mesh::from_indexed_faces({
+	Vec3(-1.0f, 1.1f, 0.0f), Vec3(0.5f, 1.0f, 0.0f), Vec3(1.1f, 0.9f, 0.0f),
+													 Vec3(0.6f, 0.4f, 0.0f),
+	Vec3(-1.1f, -0.3f, 0.0f)                       , Vec3(1.2f, -0.2f, 0.0f),
+	Vec3(-1.2f,-0.7f, 0.0f), Vec3(0.3f, -0.8f, 0.0f), Vec3(1.15f, -1.0f, 0.0f)
+	}, {
+		{0, 4, 3, 1},
+		{1, 3, 5, 2},
+		{4, 6, 7, 3},
+		{4, 7, 8, 5}
+	});
+	expect_collapse(mesh, edge, after);
+});
+
+
+/*
+EDGE CASE HARD
+
+Initial: 
+
+0---1---2
+|   |   |
+|   |   |
+|   |   |
+|   |   |
+3---4---5
+
+Collapse edge 1--4
+
+Wrong result:
+
+Mesh:
+
+.---.---.    . \   / .
+|   |   |    |  \ /  |
+|   |   | -> |   .   |
+|   |   |    |  / \  |
+.---.---.    . /   \ .
+
+REJECT!
+*/
+
+Test test_a2_l3_collapse_edge_edge_case_hard_2("a2.l2.collapse_edge.edge.case.hard.2", []() {
+	Halfedge_Mesh mesh = Halfedge_Mesh::from_indexed_faces({
+	Vec3(-1.0f, 1.1f, 0.0f), Vec3(0.5f, 1.0f, 0.0f), Vec3(1.1f, 0.9f, 0.0f),
+	Vec3(-1.5f,-0.7f, 0.0f), Vec3(0.1f, -0.8f, 0.0f), Vec3(1.4f, -1.0f, 0.0f)
+	}, {
+		{0, 3, 4, 1},
+		{1, 3, 5, 2}
+	});
+
+	Halfedge_Mesh::EdgeRef edge = mesh.halfedges.begin()->next->next->edge;
+
+	if (mesh.collapse_edge(edge)) {
+		throw Test::error("collapse_edge should not work.");
+	}
+});
